a225bcaef4e5f7810770a591dcbc2f9d528c7061
Maria Nguyen <mnguyen8@mail.pima.edu>
2015-07-25 13:17:25 -0700

    routes, models, controller, db added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @words = Word.all
+  end
+
+  def show
+    @word = Word.find(params[:id])
+  end
+
+  def new
+    @word = Word.new
+
+  end
+
+  def edit
+  end
+end
