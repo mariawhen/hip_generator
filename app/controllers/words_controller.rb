54f4a0348d119ecce0c398c673e4bdb1da788538
h4w5 <philip@h4w5.com>
2015-07-27 16:24:46 -0700

    add authentication before all word CRUD actions

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,6 @@
 class WordsController < ApplicationController
+  before_action :authenticate
+
   def index
     # @words = Word.all
     @word = Word.where(:cat => "adjective").sample.word + " " + Word.where(:cat => "noun").sample.word
@@ -47,6 +49,12 @@ class WordsController < ApplicationController
 
   def word_params
     params.require(:word).permit(:word, :cat, :tense, :genre)
+  end
+
+  def authenticate
+    unless logged_in?
+      redirect_to root_path
+    end
   end
 
 end
